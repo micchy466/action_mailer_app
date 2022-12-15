36acdc9514d72be6c33530e350ac685649f41609
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-15 16:17:47 +0900

    ✨ ユーザーテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
